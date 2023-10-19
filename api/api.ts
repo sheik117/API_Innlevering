bd9a1e2ebde0442033662533270739549bde7c98
Fillip Novak Grunnsund <sheik117@users.noreply.github.com>
2023-10-19 17:48:07 +0000

    add api

---
+++ api/api.ts
@@ -0,0 +1,44 @@
+// /pages/api/quiz
+
+const quiz = [
+    {
+      question: 'Spørsmål 1',
+      answers: [
+        {
+          answere: 'Alternativ 1',
+        },
+        {
+          answere: 'Alternativ 2',
+        },
+        {
+          answere: 'Alternativ 3',
+        },
+        {
+          answere: 'Alternativ 4',
+          correct: true,
+        },
+      ],
+    },
+    {
+      question: 'Spørsmål 2',
+      answers: [
+        {
+          answere: 'Alternativ 1',
+        },
+        {
+          answere: 'Alternativ 2',
+        },
+        {
+          answere: 'Alternativ 3',
+        },
+        {
+          answere: 'Alternativ 4',
+          correct: true,
+        },
+      ],
+    },
+  ]
+  
+  export default function handler(req, res) {
+    res.status(200).json({ success: true, data: quiz })
+  }
